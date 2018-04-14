5e3e49dea1e549407449d1ceac8541cf3a173d61
“satyanarayana <satya_eday@yahoo.com>
2018-04-13 23:53:45 -0500

    second commit

---
+++ src/test/java/com/qa/tests/HomePageTest.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+public class HomePageTest {
+	
+	public void testOneHomePage(){
+		System.out.println(" HomePage   Test test one - case  HomePage  ");
+	}
+	
+	public void testTwoHomePage(){
+		System.out.println(" HomePage  Test test TWO  - case  HomePage   ");
+	}
+
+}

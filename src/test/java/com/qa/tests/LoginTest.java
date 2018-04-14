3396c0d51088a9b7b31769c74f1aebb983234644
“satyanarayana <satya_eday@yahoo.com>
2018-04-13 23:00:04 -0500

    first commit

---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+public class LoginTest {
+	
+	public void testOne(){
+		System.out.println("Test test one - case   ");
+	}
+	
+	public void testTwo(){
+		System.out.println("Test test TWO  - case   ");
+	}
+
+}

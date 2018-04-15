501e91b995dbfc1e6f3b1d04b13e7bc2175dd532
“satyanarayana <satya_eday@yahoo.com>
2018-04-15 12:20:37 -0500

    fourth commit

--- src/test/java/com/qa/tests/GoogleHomePageTest.java
+++ src/test/java/com/qa/tests/GoogleHomePageTest.java
@@ -4,10 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleHomePageTest {
@@ -17,11 +23,28 @@ public class GoogleHomePageTest {
 	
 	
 	@BeforeMethod
-	  public void beforeMethod() {
+	@Parameters({ "browser" })
+	  public void openBrowser(String browser) {
+		
+		    
+		if (browser.equalsIgnoreCase("Firefox"))
+		  {
 		  
-		  System.setProperty("webdriver.gecko.driver", "C:\\Users\\satya\\workspace\\testSelenium\\drivers\\geckodriver.exe");
+		  System.setProperty("webdriver.gecko.driver", "C:\\Users\\satya\\Desktop\\resumes_2\\drivers\\geckodriver.exe");
 		  
 		  driver = new FirefoxDriver();
+		  
+		  }else if (browser.equalsIgnoreCase("chrome")) 
+		     {
+				System.setProperty("webdriver.chrome.driver",
+						"C:\\Users\\satya\\Desktop\\resumes_2\\drivers\\chromedriver.exe");
+				driver = new ChromeDriver();
+			} else if (browser.equalsIgnoreCase("IE")) {
+				System.setProperty("webdriver.ie.driver",
+						"C:\\Users\\satya\\Desktop\\resumes_2\\drivers\\IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+			}
+		  
 		  
 		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		  
@@ -39,8 +62,15 @@ public class GoogleHomePageTest {
 				Assert.assertEquals(getTitle, "Google");
 			}
 			
-			@AfterClass
-			public void tearDown() {
+			
+			
+			
+			
+			
+			
+			
+			@AfterMethod
+			public void closeBrowser() {
 				driver.quit();
 			}
 	
---
+++ src/test/java/com/qa/tests/grid/GridExampleTest.java
@@ -0,0 +1,45 @@
+package com.qa.tests.grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GridExampleTest {
+	
+	public RemoteWebDriver driver;
+	public static String appURL = "http://www.google.com";
+	
+	
+	@BeforeClass
+	public void setUp() throws MalformedURLException 
+	{
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void testGooglePageTitleInIEBrowser() {
+		System.out.println("*** Navigation to Application ***");
+		driver.navigate().to(appURL);
+		String strPageTitle = driver.getTitle();
+		System.out.println("*** Verifying page title ***");
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
+	}
+	
+	@AfterClass
+	public void closeBrowser() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+	
+	
+
+}
